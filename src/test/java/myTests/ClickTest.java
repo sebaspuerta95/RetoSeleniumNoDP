c902e5a3ac27bf4bc391a8415a271e9bf8cf59de
Sebastian Puerta <sepuertabe@gmail.com>
2025-02-20 10:32:23 -0500

    Added ClickTest class

---
+++ src/test/java/myTests/ClickTest.java
@@ -0,0 +1,25 @@
+package myTests;
+
+import base.TestsBase;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ClickTest extends TestsBase {
+
+    @Test
+    public void testClick(){
+        clickLink("Click");
+
+        WebElement button = driver.findElement(By.id("badButton"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(button).click().perform();
+
+        assertEquals("btn btn-success", button.getAttribute("class"), "The class is incorrect.");
+
+    }
+
+}

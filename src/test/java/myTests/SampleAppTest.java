69015e9de2b53c5d0874f0e4f714163bc9c5140e
Sebastian Puerta <sepuertabe@gmail.com>
2025-02-21 09:32:00 -0500

    The SameAppTest class was updated to include the multiple reloads test

--- src/test/java/myTests/SampleAppTest.java
+++ src/test/java/myTests/SampleAppTest.java
@@ -3,7 +3,13 @@ package myTests;
 import base.TestsBase;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,6 +20,9 @@ public class SampleAppTest extends TestsBase {
     private By passField = By.xpath("//input[@type='password']");
     private By button = By.xpath("//button[@id='login']");
 
+    /**
+     * This test reloads the page only once.
+     */
     @Test
     public void testSampleApp(){
         clickLink("Sample App");
@@ -28,6 +37,39 @@ public class SampleAppTest extends TestsBase {
 
         assertFalse(usernameId.equals(newUsernameId), "The ID of the username field didn't change dynamically.");
         assertFalse(passwordId.equals(newPasswordId), "The ID of the password field didn't change dynamically.");
+
+    }
+
+    /**
+     * This test is designed to be able to reload the page multiple time.
+     */
+    @Test
+    public void testSameAppMultipleLoads(){
+        clickLink("Sample App");
+
+        List<String> listOfUserIDs = new ArrayList<>();
+        List<String> listOfPassIDs = new ArrayList<>();
+
+        for (int i=0; i < 5; i++) {
+            try {
+                String usernameId = driver.findElement(usernameField).getAttribute("id");
+                String passwordId = driver.findElement(passField).getAttribute("id");
+
+                listOfUserIDs.add(usernameId);
+                listOfPassIDs.add(passwordId);
+
+                driver.navigate().refresh();
+
+            } catch (StaleElementReferenceException e) {
+                System.out.println("Page not reloaded properly, element became stale.");
+            }
+        }
+
+        Set<String> newListOfUserIDs = new HashSet<>(listOfUserIDs);
+        Set<String> newListOfPassIDs = new HashSet<>(listOfPassIDs);
+
+        assertEquals(newListOfUserIDs.size(), listOfUserIDs.size(), "The IDs of the usernames aren't dynamically changing.");
+        assertEquals(newListOfPassIDs.size(), listOfPassIDs.size(), "The IDs if the passwords aren't dynamically changing.");
 
     }
 
